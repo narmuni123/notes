eb7fbc36a3568422d09c6ad3b7a451fdd34d396a
narmuni123 <narmuni123@gmail.com>
2023-01-16 07:23:03 +0530

    Added exception handling

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -2,31 +2,64 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthProvider extends ChangeNotifier {
+  var registrationModel;
+  var loginModel;
+
   Future<bool> registration(
       {required context,
       required String email,
       required String password}) async {
-    final value = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    if (kDebugMode) {
-      print(value);
+    try {
+      final value = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      registrationModel = value;
+      notifyListeners();
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "weak-password") {
+        if (kDebugMode) {
+          print("weak password");
+        }
+      } else if (e.code == "email-already-in-use") {
+        if (kDebugMode) {
+          print("Email is already in use");
+        }
+      } else if (e.code == "invalid-email") {
+        if (kDebugMode) {
+          print("invalid email");
+        }
+      }
+      notifyListeners();
+      return false;
     }
-    return true;
   }
 
   Future<bool> singIn(
       {required context,
-        required String email,
-        required String password}) async {
-    final value = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    if (kDebugMode) {
-      print(value);
+      required String email,
+      required String password}) async {
+    try {
+      final value = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      loginModel = value;
+      notifyListeners();
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        if (kDebugMode) {
+          print("User not found");
+        }
+      } else if (e.code == "wrong-password") {
+        if (kDebugMode) {
+          print("Wrong password");
+        }
+      }
+      notifyListeners();
+      return false;
     }
-    return true;
   }
 }
