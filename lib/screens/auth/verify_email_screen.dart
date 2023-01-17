2a1f10bbbeb5ef3ca01932dc39face06eea5bbdf
narmuni123 <narmuni123@gmail.com>
2023-01-17 20:16:56 +0530

    Added verify email button and functionality

--- lib/screens/auth/verify_email_screen.dart
+++ lib/screens/auth/verify_email_screen.dart
@@ -1,5 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 
 class VerifyEmailScreen extends StatefulWidget {
   const VerifyEmailScreen({Key? key}) : super(key: key);
@@ -11,10 +11,33 @@ class VerifyEmailScreen extends StatefulWidget {
 class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Verify Email"),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Verify email!!"),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final user = FirebaseAuth.instance.currentUser;
+                  await user?.sendEmailVerification();
+                },
+                child: const Text("Verify email"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
