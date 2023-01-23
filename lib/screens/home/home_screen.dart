f8213921a1befb779531799de6d6879a85aa7fc7
narmuni123 <narmuni123@gmail.com>
2023-01-23 21:40:49 +0530

    Added dialog display for logout

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/provider/auth_provider.dart';
 import 'dart:developer' as devtools show log;
