ae3e7182a7f6cb1ae4bb1dac93b9e671931d250d
narmuni123 <narmuni123@gmail.com>
2023-01-24 22:21:21 +0530

    Added transition package

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: "/notesViewScreen/",
+        initialRoute: "/splash/",
         routes: {
           "/splash/": (context) => const SplashScreen(),
           "/login/": (context) => const LoginScreen(),
