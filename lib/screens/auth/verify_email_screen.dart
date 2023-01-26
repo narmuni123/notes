7999a9c00bde25e949995c24ee3e629d0fe99a73
narmuni123 <narmuni123@gmail.com>
2023-01-26 12:09:33 +0530

    Added new cnages in verifivation page email

--- lib/screens/auth/verify_email_screen.dart
+++ lib/screens/auth/verify_email_screen.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
 class VerifyEmailScreen extends StatefulWidget {
   const VerifyEmailScreen({Key? key}) : super(key: key);
@@ -12,6 +15,8 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    AuthProvider authProvider =
+        Provider.of<AuthProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -23,7 +28,22 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text("Verify email!! Email sent to you email."),
+              const Text(
+                  "We have sent you a verification email. Please open it to verify your account."),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              const Text(
+                  "If you haven't received a verification email yet, press the button below."),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  authProvider.verifyEmail(context: context);
+                },
+                child: const Text("Verify email"),
+              ),
               SizedBox(
                 height: height * 0.01,
               ),
