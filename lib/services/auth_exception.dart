2af76191df2239b8680ef0479ca392e96347bf87
narmuni123 <narmuni123@gmail.com>
2023-01-28 11:29:28 +0530

    Added register and generic exceptions

--- lib/services/auth_exception.dart
+++ lib/services/auth_exception.dart
@@ -11,3 +11,9 @@ class WeakPasswordAuthException implements Exception {}
 class EmailAlreadyInUseAuthException implements Exception {}
 
 class InvalidEmailAuthException implements Exception {}
+
+// generic exceptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
