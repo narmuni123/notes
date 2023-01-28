38a3321b013c1f2a4f18d97472e7cc6e5ec38ea6
narmuni123 <narmuni123@gmail.com>
2023-01-28 11:25:45 +0530

    Chnaged title and added login exception class

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Demo',
+        title: 'MyNotes',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
---
+++ lib/services/auth_exception.dart
@@ -0,0 +1,13 @@
+// login exception
+
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+// register exception
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
