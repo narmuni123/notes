bf082abbf73f251bb2e1be18ed6704628446a317
narmuni123 <narmuni123@gmail.com>
2023-01-30 22:34:33 +0530

    Added a packgae capitalize

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:capitalize/capitalize.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/provider/auth_provider.dart';
 import 'dart:developer' as devtools show log;
@@ -7,9 +8,9 @@ import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    IsCapitalize isCapitalize = IsCapitalize();
     AuthProvider authProvider =
         Provider.of<AuthProvider>(context, listen: false);
     return Scaffold(
