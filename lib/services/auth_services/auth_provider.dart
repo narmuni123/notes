d3d8fe1bf2e9dc28a372bc1407b2106720df4898
narmuni123 <narmuni123@gmail.com>
2023-02-01 21:24:00 +0530

    Added login in abstract class

--- lib/services/auth_services/auth_provider.dart
+++ lib/services/auth_services/auth_provider.dart
@@ -1,6 +1,10 @@
 import "package:mynotes/services/auth_services/auth_user.dart";
 
-abstract class AuthProvider{
+abstract class AuthProvider {
   AuthUser? get currentUser;
 
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  });
 }
