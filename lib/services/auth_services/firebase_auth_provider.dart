bb79fd909df2583bffaeb59c43c70df58c9a2e27
narmuni123 <narmuni123@gmail.com>
2023-02-01 21:36:57 +0530

    Added currentUser

---
+++ lib/services/auth_services/firebase_auth_provider.dart
@@ -0,0 +1,44 @@
+import 'package:mynotes/services/auth_services/auth_user.dart';
+import 'package:mynotes/services/auth_services/auth_provider.dart';
+import 'package:mynotes/services/auth_services/auth_exception.dart';
+
+import 'package:firebase_auth/firebase_auth.dart'
+    show FirebaseAuth, FirebaseAuthException;
+
+class FirebaseAuthProvider implements AuthProvider {
+  @override
+  Future<AuthUser> createUser(
+      {required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      return AuthUser.fromFirebase(user);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<AuthUser> login({required String email, required String password}) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+}
