6d82b75a1196f9bfa33e646e0ab3482e5ef65a6a
narmuni123 <narmuni123@gmail.com>
2023-02-03 23:52:22 +0530

    chnages

--- lib/provider/authentication.dart
+++ lib/provider/authentication.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/interactive_constant.dart';
 import 'package:mynotes/constants/routes.dart';
