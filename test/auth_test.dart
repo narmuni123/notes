c400470b96b52d355a589b3e46bd3ad51949d50e
narmuni123 <narmuni123@gmail.com>
2023-02-05 21:58:00 +0530

    Added a mock auth class

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,5 +1,44 @@
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:test/test.dart';
 
 void main(){
 
+}
+
+class MockAuthProvider implements AuthProvider{
+  @override
+  Future<AuthUser> createUser({required String email, required String password}) {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> initialize() {
+    // TODO: implement initialize
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthUser> login({required String email, required String password}) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendEmailVerification() {
+    // TODO: implement sendEmailVerification
+    throw UnimplementedError();
+  }
+  
 }
