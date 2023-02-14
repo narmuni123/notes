d3e51dc56841057f91be3877f0e16bb07109ec02
narmuni123 <narmuni123@gmail.com>
2023-02-14 07:51:56 +0530

    Timeout test

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -20,13 +20,24 @@ void main() {
     });
 
     test('Should be able to initialize', () async {
-      provider.initialize();
+      await provider.initialize();
       expect(provider.isInitialized, true);
     });
 
-    test('User should be null after initialization', () async {
+    test('User should be null after initialization', () {
       expect(provider.currentUser, null);
     });
+
+    test(
+      'Should be able to initialize in less than 2 seconds',
+      () async {
+        await provider.initialize();
+        expect(provider.isInitialized, true);
+      },
+      timeout: const Timeout(
+        Duration(seconds: 2),
+      ),
+    );
   });
 }
 
