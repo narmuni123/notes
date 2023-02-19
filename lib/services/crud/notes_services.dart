75d71edd491b2a8e6eef3da3294f90fd69d46f36
narmuni123 <narmuni123@gmail.com>
2023-02-19 21:38:29 +0530

    Added a db user class

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -17,6 +17,15 @@ class DatabaseUser {
   DatabaseUser.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'Person, ID = $id, email = $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
 const idColumn = 'id';
