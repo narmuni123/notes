1fbf866438236510d664bfbf2fe03d403f641d7e
narmuni123 <narmuni123@gmail.com>
2023-03-04 09:48:37 +0530

    Create new note method

--- lib/screens/notes/new_notes_view.dart
+++ lib/screens/notes/new_notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mynotes/services/auth/auth_services.dart';
+import 'package:mynotes/services/crud/notes_services.dart';
 
 class NewNotesView extends StatefulWidget {
   const NewNotesView({Key? key}) : super(key: key);
@@ -8,6 +10,21 @@ class NewNotesView extends StatefulWidget {
 }
 
 class _NewNotesViewState extends State<NewNotesView> {
+  DatabaseNotes? _notes;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  Future<DatabaseNotes> createNewNote() async {
+    final existingNote = _notes;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthServices.firebase().currentUser!;
+    final email = currentUser.email;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
