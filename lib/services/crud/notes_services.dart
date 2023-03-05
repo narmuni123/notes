a5156e4320b932339031f7a85c0fe8b6d8519719
narmuni123 <narmuni123@gmail.com>
2023-03-05 14:26:40 +0530

    Added a new broad cast with dink and add

--- lib/services/crud/notes_services.dart
+++ lib/services/crud/notes_services.dart
@@ -10,14 +10,18 @@ class NotesService {
 
   List<DatabaseNotes> _notes = [];
 
-  final _notesStreamController =
-      StreamController<List<DatabaseNotes>>.broadcast();
+  late final StreamController<List<DatabaseNotes>> _notesStreamController;
 
   Stream<List<DatabaseNotes>> get allNotes => _notesStreamController.stream;
 
   static final NotesService _shared = NotesService._sharedInstance();
 
-  NotesService._sharedInstance();
+  NotesService._sharedInstance() {
+    _notesStreamController =
+        StreamController<List<DatabaseNotes>>.broadcast(onListen: () {
+      _notesStreamController.sink.add(_notes);
+    });
+  }
 
   factory NotesService() => _shared;
 
